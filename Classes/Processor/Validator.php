b83fa671c5259fc1fd52423ab359b6fe0b8db887
Axel Seemann-Kahne <info@seemann-kahne.de>
2026-06-19 12:44:47 +0200

    fix: add docblock for Validator class

--- Classes/Processor/Validator.php
+++ Classes/Processor/Validator.php
@@ -6,6 +6,11 @@ namespace Ask\AskBatchImporter\Processor;
 
 final class Validator
 {
+    /**
+     * @param array $record
+     * @param array $requiredSourceFields
+     * @return void
+     */
     public function validate(array $record, array $requiredSourceFields): void
     {
         foreach ($requiredSourceFields as $field) {
